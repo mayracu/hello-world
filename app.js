332e96e8042a4396b5120019838193f3e04d383f
MayraCuellar <mayrabeyota@hotmail.com>
2023-01-30 04:49:36 +0000

    Dynamic Components added

--- app.js
+++ app.js
@@ -57,21 +57,28 @@
 //     alert("Hi! Welcome to my coffee blog.");
 // }
 
-    
+ 
 
 function coffeeCups(){
     function sugarQuestion(){
-        let coffeeSugar = prompt("You are definititely a coffee lover! Do you add sugar to your coffee?");
+        let coffeeSugar = prompt("You are definititely a coffee lover! Do you add sugar to your coffee? (Type yes or no)");
+            while (coffeeSugar != "yes" && coffeeSugar != "no") {
+            coffeeSugar = prompt("You are definititely a coffee lover! Do you add sugar to your coffee? (Type yes or no)");
+            } 
             if (coffeeSugar == "yes"){
-                alert("You like your coffee sweet! I'm glad you are here. Enjoy my site.");
+                alert("You like your coffee sweet!");
+                CoffeeChoice();
             }
             else if(coffeeSugar == "no"){
-                alert("You are a coffee purist! I'm glad you are here. Enjoy my site.");
+                alert("You are a coffee purist!");
+                CoffeeChoice();
             }
-            else {
-                alert("I'm glad you are here. Enjoy my site.");
-            }
+            // else {
+            //     let coffeeSugar = prompt("Please, only respond with yes or no. Do you add sugar to your coffee?");
+            //     // alert("I'm glad you are here. Enjoy my site.");
+                
     }
+
     let cupsNumber= prompt("How many cups of coffee do you drink a day?");
       if (cupsNumber == 0){
         alert("You stay alert without coffee. That's a superpower.");
@@ -103,27 +110,52 @@ function teaQuestion(){
 
 let welcomeMessage= prompt("Hi! Welcome to my blog! Are you a coffee lover? Respond yes or no");
     //console.log(welcomeMessage);
+    while (welcomeMessage != "yes" && welcomeMessage != "no") {
+        welcomeMessage = prompt("Please, only respond with yes or no. Are you a coffee lover?");
+    } 
+
     if (welcomeMessage == "yes"){
         coffeeCups();
     }
     else if (welcomeMessage == "no"){
         teaQuestion();
     }
-    else {
-        let welcomeMessage2= prompt("Please, only respond with yes or no. Are you a coffee lover?");
-        if (welcomeMessage2 == "yes"){
-            coffeeCups();
-        }
-        else if (welcomeMessage2 == "no"){
-            teaQuestion();
-        }
-        else {
-            let text = "To continue press a button!\nEither OK or Cancel.";
-                if (confirm(text) == true) {
-                    alert("Welcome!");
-                }
-                else {
-                    alert("Welcome!");
-                }       
+    // else {
+    //     let welcomeMessage2= prompt("Please, only respond with yes or no. Are you a coffee lover?");
+    //     if (welcomeMessage2 == "yes"){
+    //         coffeeCups();
+    //     }
+    //     else if (welcomeMessage2 == "no"){
+    //         teaQuestion();
+    //     }
+    //     // else {
+        //     let text = "To continue press a button!\nEither OK or Cancel.";
+        //         if (confirm(text) == true) {
+        //             alert("Welcome!");
+        //         }
+        //         else {
+        //             alert("Welcome!");
+        //         }            
+        // }
+//
+
+function CoffeeChoice(){
+let userPreference = prompt("Would you prefer a cappuccino or an americano? (Respond with the initials c for cappuccino or a for americano");
+    while (userPreference != "c" && userPreference != "a") {
+        userPreference = prompt("Would you prefer a cappuccino or an americano? (Respond with the initials c for cappuccino or a for americano)");
+    }  
+    if (userPreference == "c"){
+        numCappuccino = prompt("How many cappuccinos would you like? (0~5)");
+        document.write("<p> You like cappuccinos! </p>");
+        for(let i=0; i < numCappuccino; i++){
+            document.write("<img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.IIpYHS9y5YnWOoL7TYtSswHaE8%26pid%3DApi&f=1&ipt=15276d4187cc53b42becb4d0eb4204d4a3dc0d10949ffda1cf038fff66bbe04e&ipo=images' alt='cappuccino'/>");
+        }    
+    } 
+    else if (userPreference == "a"){
+        numAmericano = prompt("How many coffee americano would you like? (0~5)");
+        document.write("<p id='americano-preference'> You like coffee americano! </p>");
+        for(let i=0; i < numAmericano; i++){
+            document.write("<img src='https://c.pxhere.com/photos/80/51/cup_coffee_pires_cup_of_coffee_breakfast_porcelain_cup_porcelain_cup_of_cafe-635116.jpg!s' alt='coffee americano' />");
         }
     }
+}
